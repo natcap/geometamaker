db717aab2cd790bf40b2e2a2fdb1a1212306a4df
davemfish <davemfish@gmail.com>
2024-12-11 16:43:48 -0500

    added cli entrypoint for configuring geometamaker. #43

--- src/geometamaker/cli.py
+++ src/geometamaker/cli.py
@@ -47,5 +47,41 @@ def validate(filepath, recursive):
             click.echo(validation_message)
 
 
+def print_config(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    config = geometamaker.Config()
+    click.echo(f'{config.profile}')
+    ctx.exit()
+
+
+@click.command()
+@click.option('--individual_name', prompt=True, default='')
+@click.option('--email', prompt=True, default='')
+@click.option('--organization', prompt=True, default='')
+@click.option('--position_name', prompt=True, default='')
+@click.option('--license_title', prompt=True, default='')
+@click.option('--license_path', prompt=True, default='')
+@click.option('-p', '--print', is_flag=True, is_eager=True, callback=print_config, expose_value=False)
+def config(individual_name, email, organization, position_name,
+           license_path, license_title):
+    contact = geometamaker.models.ContactSchema()
+    contact.individual_name = individual_name
+    contact.email = email
+    contact.organization = organization
+    contact.position_name = position_name
+
+    license = geometamaker.models.LicenseSchema()
+    license.path = license_path
+    license.title = license_title
+
+    profile = geometamaker.models.Profile(contact=contact, license=license)
+    # click.echo(f'{profile}')
+    config = geometamaker.Config()
+    config.save(profile)
+    click.echo(f'saved profile information to {config.config_path}')
+
+
 cli.add_command(describe)
 cli.add_command(validate)
+cli.add_command(config)
