bee0bd0427ca90e9b58a11626ef4bbe1da0667e4
davemfish <davemfish@gmail.com>
2024-12-11 14:12:59 -0500

    switched to click for cli. #43

--- src/geometamaker/cli.py
+++ src/geometamaker/cli.py
@@ -1,64 +1,51 @@
-import argparse
+# import argparse
 import logging
 import os
 import sys
 
+import click
+
 import geometamaker
 
-
-def main(user_args=None):
-    parser = argparse.ArgumentParser(
-        description=(
-            ''),
-        prog='geometamaker'
-    )
-
-    subparsers = parser.add_subparsers(dest='subcommand')
-    describe_subparser = subparsers.add_parser(
-        'describe', help='describe a dataset')
-    describe_subparser.add_argument(
-        'filepath',
-        help=('path to a dataset to describe'))
-    validate_subparser = subparsers.add_parser(
-        'validate', help='validate a metadata document')
-    validate_subparser.add_argument(
-        'filepath',
-        help=('path to a metadata document, or directory containing documents, to validate'))
-    validate_subparser.add_argument(
-        '-r', '--recursive', action='store_true', default=False,
-        help='recurse through subdirectories in search of metadata documents.')
-
-    args = parser.parse_args(user_args)
-
-    # root_logger = logging.getLogger()
-    handler = logging.StreamHandler(sys.stdout)
-    formatter = logging.Formatter(
-        fmt='%(asctime)s %(name)-18s %(levelname)-8s %(message)s',
-        datefmt='%m/%d/%Y %H:%M:%S ')
-    handler.setFormatter(formatter)
-    handler.setLevel(logging.DEBUG)  # TODO: take user input
-
-    if args.subcommand == 'describe':
-        if os.path.isdir(args.filepath):
-            geometamaker.describe_dir(
-                args.filepath, recursive=args.recursive)
-        else:
-            geometamaker.describe(args.filepath).write()
-        # sys.stdout.write(pprint.pformat(description))
-        parser.exit()
-
-    if args.subcommand == 'validate':
-        if os.path.isdir(args.filepath):
-            file_list, message_list = geometamaker.validate_dir(
-                args.filepath, recursive=args.recursive)
-            for filepath, msg in zip(file_list, message_list):
-                sys.stdout.write(f'{filepath}: {msg}\n')
-        else:
-            validation_message = geometamaker.validate(args.filepath)
-            if validation_message:
-                sys.stdout.write(validation_message)
-        parser.exit()
+# root_logger = logging.getLogger()
+handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter(
+    fmt='%(asctime)s %(name)-18s %(levelname)-8s %(message)s',
+    datefmt='%m/%d/%Y %H:%M:%S ')
+handler.setFormatter(formatter)
+handler.setLevel(logging.DEBUG)  # TODO: take user input
 
 
-if __name__ == '__main__':
-    main()
+@click.group()
+def cli():
+    pass
+
+
+@click.command()
+@click.argument('filepath')
+@click.option('-r', '--recursive', is_flag=True, default=False)
+def describe(filepath, recursive):
+    if os.path.isdir(filepath):
+        geometamaker.describe_dir(
+            filepath, recursive=recursive)
+    else:
+        geometamaker.describe(filepath).write()
+
+
+@click.command()
+@click.argument('filepath')
+@click.option('-r', '--recursive', is_flag=True, default=False)
+def validate(filepath, recursive):
+    if os.path.isdir(filepath):
+        file_list, message_list = geometamaker.validate_dir(
+            filepath, recursive=recursive)
+        for filepath, msg in zip(file_list, message_list):
+            click.echo(f'{filepath}: {msg}\n')
+    else:
+        validation_message = geometamaker.validate(filepath)
+        if validation_message:
+            click.echo(validation_message)
+
+
+cli.add_command(describe)
+cli.add_command(validate)
